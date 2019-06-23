095f5f5021d816edcfecf1b9edecb6a7c81d1ae3
ethomascappello <ethomascappello@Marist-HNSQXQ2.necsd.net>
2019-06-23 14:37:06 -0400

    add perimeter and area

--- src/ExerciseEight.java
+++ src/ExerciseEight.java
@@ -2,7 +2,11 @@
 public class ExerciseEight {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//perimeter
+		System.out.println (2 * 5.5 * 3.14591);
+		//area
+		System.out.println (5.5 * 5.5 * 3.14159);
 
 	}
 
